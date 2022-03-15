1f8b9bc2bf8555f2b6b8ea5759c72d9ab0d801ed
Julius <ibejulius1@gmail.com>
2022-03-15 17:34:43 +0100

    readme

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,20 @@
-#include <stdio.h>
-
-#include <unistd.h>
+. #include <stdio.h>
 /**
- * main entry point
- *
- * Return: Always 1 (success)
- */
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-write(2,"and that piece of art is useful\"-Dora Korpar,2015-10-19\n",59);
-return (1);
+int i;
+long int i2;
+long long int i3;
+char c;
+float f;
+
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(i2));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(i3));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
 }
