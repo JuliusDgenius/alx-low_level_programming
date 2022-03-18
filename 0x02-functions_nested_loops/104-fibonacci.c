f5666f46e1cd485a73a6982119a646046c3a2e08
Julius <ibejulius1@gmail.com>
2022-03-18 05:30:10 +0100

    A program that finds and prints the first 98 Fibo nums, starting w/ 1 & 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -16,7 +16,7 @@ unsigned long num1, num2, t, rem, fp_num2, sp_num2, fp_num1, sp_num1, t1, t2;
 num1 = 0;
 num2 = 1;
 flag = 0;
-for (i = 0; i < 98; i++)
+for (i = 0; i < 91; i++)
 {
 if (num2 < 1000000000000000000)
 {
@@ -39,7 +39,7 @@ t2 = sp_num1 + sp_num2;
 rem = t2 / 10000000000;
 t1 += rem;
 t2 %= 10000000000;
-if (i < 97)
+if (i < 99)
 printf("%lu%010lu, ", t1, t2);
 fp_num1 = fp_num2;
 sp_num1 = sp_num2;
