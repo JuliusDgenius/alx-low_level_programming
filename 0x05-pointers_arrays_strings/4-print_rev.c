ff57e1a4202b0c78e9221cdf470a746d5dc2e4ab
Julius <ibejulius1@gmail.com>
2022-03-23 06:30:00 +0100

    prints a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,23 @@
 #include "main.h"
 
 /**
- * print_rev - prints string backwards
+ * print_rev - print string backwards
  * @s: string
- * Return: Nothing
+ * Return: Nothing.
  */
+
 void print_rev(char *s)
 {
-int counter = 0;
-while (counter >= 0)
-if (s[counter] == '\0')
-break;
-counter++;
+	int counter = 0;
+
+	while (counter >= 0)
+	{
+		if (s[counter] == '\0')
+			break;
+		counter++;
+	}
+
+	for (counter--; counter >= 0; counter--)
+		_putchar(s[counter]);
+	_putchar('\n');
 }
-for (counter--; counter >= 0; counter--)
-_putchar(s[counter]);
-_putchar('\n');
