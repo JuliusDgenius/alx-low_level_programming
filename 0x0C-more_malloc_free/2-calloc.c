88b3cf1206a49c34f85f62d9c12ec5a6467085fc
Julius <ibejulius1@gmail.com>
2022-04-12 06:30:37 +0100

    A function that allocates memory for an array, usimg malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,11 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * _calloc - allocates memory
- * @nmemb: input string
- * @size: input string
- * Return: Pointer
+ * _calloc - prints calloc;
+ * @nmemb: input string.
+ * @size: input string.
+ * Return: Pointer.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
@@ -14,15 +13,15 @@ unsigned int i, d;
 
 d = nmemb * size;
 if (nmemb <= 0 || size <= 0)
-	return (NULL);
+return (NULL);
 
 p = malloc(d);
 if (p == NULL)
-	return (NULL);
+return (NULL);
 
-for (1 = 0; i < (d); i++)
+for (i = 0; i < (d); i++)
 {
-	p[i] = 0;
+p[i] = 0;
 }
 return (p);
 }
