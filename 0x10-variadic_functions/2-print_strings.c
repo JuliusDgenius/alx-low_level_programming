bd4f07ae56a60534b57e526d2720511cc306026d
Julius <ibejulius1@gmail.com>
2022-04-18 00:41:27 +0100

    A function that prints s string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,7 +9,7 @@
  *
  * Return: void.
  */
-void print_strings(const cahr *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 unsigned int i;
 char *str;
