f7fffa6df97604c32ecb03d20068dd265c102f8d
Julius <ibejulius1@gmail.com>
2022-04-21 13:31:33 +0100

    function prototypes

--- 0x10-variadic_functions/function_pointers.h
+++ 0x10-variadic_functions/function_pointers.h
@@ -1,5 +1,4 @@
 int _putchar(char c);
-void print_name(char *name, void (*f)(char *));
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
