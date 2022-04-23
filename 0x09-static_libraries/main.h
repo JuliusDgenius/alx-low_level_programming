bf85fdde4a4d3af1b52069d16aeae61a86cd2d88
JuliusDgenius <ibejulius1@gmail.com>
2022-04-23 19:11:24 +0100

    main header file

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -22,3 +22,8 @@ char *_strstr(char *haystack, char *needle);
 void print_alphabet_x10(void);
 int print_sign(int n);
 int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
