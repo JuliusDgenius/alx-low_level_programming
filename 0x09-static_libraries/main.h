014a117b9029dda4d64f87bab24795bed22ffa05
JuliusDgenius <ibejulius1@gmail.com>
2022-04-24 02:22:57 +0100

    main header file

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,4 @@
 int _putchar(char c);
-void print_alphabet(void);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
@@ -19,11 +18,3 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-void print_alphabet_x10(void);
-int print_sign(int n);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
