a40e6402aeb52327e16ade08d7eeda74ed8cc160
JuliusDgenius <ibejulius1@gmail.com>
2023-07-08 12:32:44 +0100

    task 3

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at an index in a decimal number
+ * @n: number to scratch
+ * @index: index of the bit
+ *
+ * Return: value of the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int bit_num;
+
+	if (index > 64)
+		return -1;
+	bit_num = (n >> 1) & 1;
+
+	return (bit_num);
+}
