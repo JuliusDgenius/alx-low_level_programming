ca46cbfe7a1d17c74f1d0750e28aae34c23281c3
JuliusDgenius <ibejulius1@gmail.com>
2023-08-18 04:52:20 +0100

    Task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,35 +10,35 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *node = NULL;
-    unsigned int i = 0;
+dlistint_t *node = NULL;
+unsigned int i = 0;
 
-    if (head != NULL)
-    {
-        node = *head;
-        while ((i != index) && (node != NULL))
-        {
-            node = node->next;
-            i++;
-        }
-        if ((i == index) && (node != NULL))
-        {
-            if (node->next != NULL)
-            {
-                node->next->prev = node->prev;
-            }
-            if (node->prev != NULL)
-            {
-                node->prev->next = node->next;
-            }
-            if (i == 0)
-            {
-                *head = node->next;
-            }
-            free(node);
-            node = NULL;
-            return (1);
-        }
-    }
+if (head != NULL)
+{
+node = *head;
+while ((i != index) && (node != NULL))
+{
+node = node->next;
+i++;
+}
+if ((i == index) && (node != NULL))
+{
+if (node->next != NULL)
+{
+node->next->prev = node->prev;
+}
+if (node->prev != NULL)
+{
+node->prev->next = node->next;
+}
+if (i == 0)
+{
+*head = node->next;
+}
+free(node);
+node = NULL;
+return (1);
+}
+}
     return (-1);
 }
