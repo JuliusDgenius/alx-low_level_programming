c6b01fb3c0775e92bb4d958d5a1f67149e41f47f
JuliusDgenius <ibejulius1@gmail.com>
2023-08-26 04:47:00 +0100

    make me win script

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "9 8 10 24 75 9"
-echo "Congratulations, you win the Jackpot!"
+LD_PRELOAD=//home/juliusdgenius/JuliusDgenius/alx-low_level_programming/0x18-dynamic_libraries/101-make_me_win.so
