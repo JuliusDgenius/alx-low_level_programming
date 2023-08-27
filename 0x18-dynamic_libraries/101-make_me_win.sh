ddc531233de7ea5d1f2224dd2330f59b5dff768c
JuliusDgenius <ibejulius1@gmail.com>
2023-08-27 07:40:52 +0100

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-echo "9 8 10 24 75 9"
-LD_PRELOAD=//home/juliusdgenius/JuliusDgenius/alx-low_level_programming/0x18-dynamic_libraries/101-make_me_win.so
+LD_PRELOAD=/$PWD/101-make_me_win.so ./gm 9 8 10 24 75 9
