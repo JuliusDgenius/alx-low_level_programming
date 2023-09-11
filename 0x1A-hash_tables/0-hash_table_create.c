d0051c0bd83deff52498e2e35bb6d96b03940814
JuliusDgenius <ibejulius1@gmail.com>
2023-09-11 05:18:50 +0100

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -34,7 +34,8 @@ hash_table_t *hash_table = (hash_table_t *) malloc(sizeof(hash_table_t));
 if (hash_table == NULL)
 return (NULL);
 hash_table->size = size;
-hash_table->array = calloc(hash_table->size, sizeof(hash_node_t*));
+hash_table->array = (hash_table->array **) calloc(hash_table->size,
+        sizeof(hash_node_t*));
 if (hash_table->array == NULL)
     return (NULL);
 for (i = 0; i < hash_table->size; i++)
