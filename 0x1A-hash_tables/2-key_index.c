1a2a7a58cffda3f64a323b500add3228d14eeba7
JuliusDgenius <ibejulius1@gmail.com>
2023-09-11 05:31:12 +0100

    Task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,11 +5,12 @@
  *
  * @key: The key which index to return
  * @size: The size of the array of the hash table
+ * Return: returns unsigned long integer
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int index;
+unsigned long int index;
 
-    index = hash_djb2(key);
-    return (index % size);
+index = hash_djb2(key);
+return (index % size);
 }
